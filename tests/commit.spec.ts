369068aab97eac48051917bb2c8ed2d2bfb43611
DavidUhlíř <posta@daviduhlir.cz>
2024-05-22 20:42:58 +0200

    read test

--- tests/commit.spec.ts
+++ tests/commit.spec.ts
@@ -20,4 +20,15 @@ describe('Commit scope tests', function() {
     const content = fs.readFileSync('./temp//dir/file.txt').toString()
     assert(content === 'Hello', 'File content should be written on disk')
   })
+
+  it('Real read', async function() {
+    let content
+    await FileScope.prepare('./temp', {
+      a: FileScope.readAccess('/dir/file.txt'),
+    }).open(async (fs, dependecies) => {
+      content = (await dependecies.a.fs.readFile()).toString()
+    })
+
+    assert(content === 'Hello', 'File content should be readable from disk')
+  })
 })
