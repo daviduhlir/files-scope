e16862d5499f70760bbbd80baf199357bc4b77e3
DavidUhlíř <posta@daviduhlir.cz>
2024-05-23 11:46:10 +0200

    prepare for release

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,11 +1,10 @@
-import { FileScope } from '@david.uhlir/files-scope';
+import { Dependency, FileScope } from '@david.uhlir/files-scope';
 
 const main = async () => {
   await new FileScope('./temp', {
-    testFile: FileScope.writeAccess('/main/test.txt')
+    testFile: Dependency.readFileAccess('/main/test.txt')
   }).open(async (fs, dependecies) => {
     await dependecies.testFile.fs.writeFile('hello world')
-    // await fs.promises.writeFile(dependecies.testFile.filePath, 'Hello world')
   })
 }
 main();
